7bf29434cd6b65c123550ecd6c8e4d70d34f8df0
Tavi Craciunescu <ccraciun@gmail.com>
2014-04-25 11:07:32 -0700

    wip.

---
+++ js/dipEngine.js
@@ -0,0 +1,68 @@
+﻿$(function () {
+    var map = new DipMap('#map');
+    var statusBox = new StatusBox('#messageBox');
+
+    function countStatus(state) {
+        var counts = {};
+
+        // Update SC state.
+        for (sc in state.SC) {
+            if (!(state.SC[sc] in counts)) {
+                counts[state.SC[sc]] = {
+                    SCs: 0,
+                    armies: 0,
+                    fleets: 0
+                }
+            }
+            counts[state.SC[sc]].SCs++;
+        };
+
+        for (power in state.forces) {
+            counts[power].armies = state.forces[power].armies.length;
+            counts[power].fleets = state.forces[power].fleets.length;
+        };
+
+        // Update status bar.
+        for (pow in counts) {
+            statusBox.putLine(pow + ' has ' + counts[pow].SCs + ' SCs, ' +
+                    counts[pow].armies + ' armies, ' +
+                    counts[pow].fleets + ' fleets.', pow);
+        };
+
+        return counts;
+    };
+
+    // TODO(ccraciun): Ideally we would use jDip map data.
+    jQuery.getJSON('data/europe_standard_defs.json')
+        .done(function (data) {
+            map.loadDefs(data);
+        })
+        .fail(jQueryAjaxErrorHandler);
+
+    map.loadMapFromUrl('img/europe_standard.svg');
+    loadjscssfile('css/europe_standard.css', 'css');
+    jQuery.getJSON('data/europe_standard_start.json')
+        .done(function (data) {
+            // TODO(ccraciun): We need defs to be loaded before this is called,
+            // but this is not enforced.
+            map.setState(data);
+            countStatus(data);
+            map.listenOrders('Austria');
+        })
+        .fail(jQueryAjaxErrorHandler);
+});
+
+// TODO(ccraciun): Rename to history box.
+function StatusBox(statusBoxSelector) {
+    var statusBox = jQuery(statusBoxSelector);
+
+    this.putLine = function (text, source) {
+        if (!source) {
+            source = '';
+        }
+        jQuery('<div/>', {
+            'class': 'message ' + source,
+            text: text
+        }).appendTo(statusBox);
+    };
+};
--- js/dipMap.js
+++ js/dipMap.js
@@ -1,17 +1,15 @@
 ﻿function DipMap(mapSelector) {
     // The map frontend
     // TODO(ccraciun): patch Snap to support loading defs.
-    var map = Snap(mapSelector);
-    var defs = null;
+    var map = Snap(mapSelector), defs = null, instanceDipMap = this;
 
     this.getMap = function () { return map };
 
     function onLoadRawSvg(rawMap) {
         map.append(rawMap.select('defs'));
         map.append(rawMap.select('g#MapLayer'));
-        map.group().attr({
-            id: 'ForcesLayer'
-        });
+        map.group().attr({id: 'ForcesLayer'});
+        map.group().attr({id: 'OrdersLayer'});
     };
 
     this.loadMap = function (mapSvg) {
@@ -28,27 +26,28 @@
 
     this.placeItem = function (container, type, loc) {
         var it = map.select('defs ' + type);
-        it.use().transform('translate(' + defs.coords[loc] + ')')
+        it.use().attr({'class': 'force'})
+                .transform('translate(' + defs.coords[loc] + ')')
+                .appendTo(container);
+    };
+
+    this.placeItem2 = function (power, type, loc) {
+        var forces = map.select('g#ForcesLayer');
+        var power = forces.select('g#' + power);
+        if (!power) {
+            power = forces.group().attr({id: power});
+        }
+        var it = map.select('defs ' + type);
+        it.use().attr({'class': 'force'})
+                .data({loc: loc, type: type, power: power})
+                .transform('translate(' + defs.coords[loc] + ')')
                 .appendTo(container);
     };
 
     this.setState = function (state) {
-        // TODO(ccraciun): Move counting into separate function.
-        var counts = {}
-        defs.belligerents.map(function(pow) {
-            counts[pow] = {
-                SCs: 0,
-                armies: 0,
-                fleets: 0
-            }
-        });
-
         // Update SC state.
         for (sc in state.SC) {
             map.select('#sc' + sc).node.setAttribute('class', state.SC[sc]);
-            if(state.SC[sc] in counts) {
-                counts[state.SC[sc]].SCs++;
-            }
         };
 
         // Update forces
@@ -60,16 +59,72 @@
                 this.placeItem.partial(gPow, '#A'));
             state.forces[power].fleets.map(
                 this.placeItem.partial(gPow, '#F'));
-
-            counts[power].armies = state.forces[power].armies.length;
-            counts[power].fleets = state.forces[power].fleets.length;
         };
+    };
 
-        // Update status bar.
-        for (pow in counts) {
-            console.log('Power ' + pow + ' has ' + counts[pow].SCs + ' SCs, ' +
-                    counts[pow].armies + ' armies, ' +
-                    counts[pow].fleets + ' fleets.');
+    this.drawOrders = function (orders) {
+        var ordersLayer = map.select('g#OrdersLayer');
+        for (unit in orders) {
+            var unit = orders[unit].unit,
+                src = orders[unit].src,
+                act = orders[unit].act,
+                dst1 = orders[unit].dst1,
+                dst2 = orders[unit].dst2,
+                pow = orders[unit].pow;
+            // TODO(ccraciun): Handle orders other than move.
+            if (act == 'move') {
+                ordersLayer.path('M' + defs.coords[src] + 'L' + defs.coords[dst1])
+                           .attr({'marker-end': 'url(#head_success)',
+                                  'class': 'order ' + pow});
+            };
+        };
+    };
+
+    this.clearOrders = function () {
+        var ordersLayer = map.select('g#OrdersLayer');
+        ordersLayer.selectAll('.order').remove();
+    };
+
+    this.listenOrders = function (power, toggle) {
+        var orders = {}, currentOrder = {};
+        // Add listeners to the forces of given power.
+        map.selectAll('g#ForcesLayer #' + power + ' .force')
+           .forEach(function (e) {
+               e.click(function (evt) {
+                   console.log('Got a click on ' + evt.target);
+                   currentOrder.unit = evt.target;
+               })
+        });
+        // Add listeners to the territories.
+        var regionClick = function (evt) {
+            console.log('Got a click on ' + evt.target);
+            tgt = evt.target.parentNode.id;
+            if (!currentOrder.unit) {
+                return;
+            };
+            if (!currentOrder.src) {
+                currentOrder.src = tgt;
+            } else if (!currentOrder.dst1) {
+                currentOrder.dst1 = tgt;
+            } else if (!currentOrder.dst2) {
+                currentOrder.dst2 = tgt;
+                currentOrder.act = 'move';
+                orders[currentOrder.unit] = currentOrder;
+                console.log(orders);
+                instanceDipMap.clearOrders();
+                instanceDipMap.drawOrders(orders);
+                currentOrder = {};
+            };
+        };
+        map.selectAll('g#MapLayer .l').forEach(function (e) {
+            e.click(regionClick);
+        });
+        map.selectAll('g#MapLayer .w').forEach(function (e) {
+            e.click(regionClick);
+        });
+
+        return function () {
+            return orders;
         };
     };
 };
