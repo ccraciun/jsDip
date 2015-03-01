817a429f2790d36da6617a5ba9e7c28edf9f722d
Octavian <ccraciun@gmail.com>
2015-02-28 18:45:04 -0800

    Creating judge functions in a loop broke some closures.

--- test/spec/test_judge.js
+++ test/spec/test_judge.js
@@ -15,17 +15,30 @@ var root = (typeof global !== "undefined" && global !== null) ? global : this;
 
     global.defs = new dfs.Defs(JSON.parse(fs.readFileSync('app/data/europe_standard_defs.json', 'utf8')));
 
+    var judgesCorrectly = function (order) {
+        (order.succeeds()).should.equal(order.test_expectedSucceeds);
+    };
+
+    var judgesNoFailWhyOnSuccess = function (order) {
+        if (order.test_expectedSucceeds) {
+            if(order.whyFail) {
+                console.log(JSON.stringify(order, null, 2));
+            }
+            should.not.exist(order.whyFail);
+        }
+    };
+
     describe('Judge', function () {
         var doc = yaml.safeLoad(fs.readFileSync('test/data/rule_tests.yml', 'utf8'));
-        for (var testnum in doc) {
-            describe('for Testcase ' + doc[testnum].testCaseID, function () {
-                var state = new st.State(doc[testnum].state);
+        for (var testNum in doc) {
+            describe('for Testcase ' + doc[testNum].testCaseID, function () {
+                var state = new st.State(doc[testNum].state);
                 var orders = [];
-                for (var power in doc[testnum].orders) {
-                    for (var orderStr in doc[testnum].orders[power]) {
+                for (var power in doc[testNum].orders) {
+                    for (var orderStr in doc[testNum].orders[power]) {
                         var order = ord.Order.fromString(orderStr);
                         order.owner = power;
-                        order.test_expectedSucceeds = doc[testnum].orders[power][orderStr];
+                        order.test_expectedSucceeds = doc[testNum].orders[power][orderStr];
                         orders.push(order);
                     }
                 }
@@ -36,13 +49,11 @@ var root = (typeof global !== "undefined" && global !== null) ? global : this;
 
                 for (var orderNum in orders) {
                     var o = orders[orderNum];
-                    it('should judge order ' + o.str + ' correctly', function () {
-                        (o.succeeds()).should.equal(o.test_expectedSucceeds);
-                    });
+                    var fun = _.partial(judgesCorrectly, o);
+                    it('should judge order ' + o.str + ' correctly', fun);
                     if (o.test_expectedSucceeds) {
-                        it('should have no fail reasons for order ' + o.str, function () {
-                            should.not.exist(o.whyFail);
-                        });
+                        fun = _.partial(judgesNoFailWhyOnSuccess, o);
+                        it('should have no fail reasons for successful order ' + o.str, fun);
                     }
                 }
             });
