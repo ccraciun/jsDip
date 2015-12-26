50d2591eda78178cfe1b0c82403210972a98b981
Octavian <ccraciun@gmail.com>
2015-12-26 15:21:16 -0800

    remove stray console.log

--- test/spec/test_judge.js
+++ test/spec/test_judge.js
@@ -35,7 +35,6 @@ var Defs = require('../../app/scripts/models/defs');
                     for (var orderStr in doc[testNum].orders[power]) {
                         var order = Order.fromString(orderStr);
                         order.owner = power;
-                        console.log(doc[testNum].orders[power][orderStr]);
                         order.test_expectedSucceeds = doc[testNum].orders[power][orderStr][0];
                         orders.push(order);
                     }
